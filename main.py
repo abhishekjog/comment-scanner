91114c7b6a2c5f1dcbebea45cae6622df018a684
Abhishek Jog <49052188+abhishekjog@users.noreply.github.com>
2021-04-11 19:55:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,7 @@ import requests
 
 GITHUB_API_BASE_URL = 'https://api.github.com'
 
-
-def escape(v: str) -> str:
-    return repr(v)[1:-1]
-
-
-def set_action_output(name: str, value: str):
-    sys.stdout.write(f'::set-output name={name}::{escape(value)}\n')
-
-
-def print_action_error(msg: str):
-    sys.stdout.write(f'::error file={__name__}::{escape(msg)}\n')
-
-
-def print_action_debug(msg: str):
-    sys.stdout.write(f'::debug file={__name__}::{escape(msg)}\n')
-
-
-def get_action_input(
-    name: str, required: bool = False, default: Optional[str] = None
-) -> str:
+def get_action_input(name: str, required: bool = False, default: Optional[str] = None) -> str:
     v = os.environ.get(f'INPUT_{name.upper()}', '')
     if v == '' and default:
         v = default
@@ -34,50 +15,6 @@ def get_action_input(
         print_action_error(f'input required and not supplied: {name}')
         exit(1)
     return v
-
-
-def create(token, repo, body, issue_number) -> Tuple[str, str]:
-    headers = {
-        'Authorization': f'token {token}',
-    }
-    data = {
-        'body': body,
-    }
-    resp = requests.post(
-        f'{GITHUB_API_BASE_URL}/repos/{repo}/issues/{issue_number}/comments',
-        headers=headers,
-        json=data,
-    )
-    if resp.status_code != HTTPStatus.CREATED:
-        print_action_error(f'cannot create comment')
-        print_action_debug(f'status code: {resp.status_code}')
-        print_action_debug(f'response body: {resp.text}')
-        exit(1)
-
-    json = resp.json()
-    return str(json['id']), body
-
-
-def edit(token, repo, body, comment_id) -> Tuple[str, str]:
-    headers = {
-        'Authorization': f'token {token}',
-    }
-    data = {
-        'body': body,
-    }
-    resp = requests.patch(
-        f'{GITHUB_API_BASE_URL}/repos/{repo}/issues/comments/{comment_id}',
-        headers=headers,
-        json=data,
-    )
-    if resp.status_code != HTTPStatus.OK:
-        print_action_error(f'cannot edit comment')
-        print_action_debug(f'status code: {resp.status_code}')
-        print_action_debug(f'response body: {resp.text}')
-        exit(1)
-
-    json = resp.json()
-    return str(json['id']), body
 
 def delete(token, repo, comment_id) -> Tuple[str, str]:
     headers = {
@@ -93,8 +30,6 @@ def delete(token, repo, comment_id) -> Tuple[str, str]:
         print_action_debug(f'response body: {resp.text}')
         exit(1)
 
-    return '', ''
-
 def main():
     repo = os.environ['GITHUB_REPOSITORY']
     action_type = get_action_input('type', required=True)
@@ -109,15 +44,5 @@ def main():
         _id, _body = delete(token, repo, comment_id)
         exit(1)
         
-    #_id, _body = '', ''
-    #if action_type == 'create':
-    #    _id, _body = create(token, repo, body, issue_number)
-    #elif action_type == 'edit':
-    #    _id, _body = edit(token, repo, body, comment_id)
-
-    set_action_output('id', _id)
-    set_action_output('body', _body)
-
-
 if __name__ == '__main__':
     main()
