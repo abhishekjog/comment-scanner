3303f3d7493b7ac3a375f962a3be96e21ae3432b
Abhishek Jog <49052188+abhishekjog@users.noreply.github.com>
2021-04-11 20:31:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ import requests
 
 GITHUB_API_BASE_URL = 'https://api.github.com'
 
-def get_action_input(name: str, required: bool = False, default: Optional[str] = None) -> str:
+def print_error(msg: str):
+    sys.stdout.write(f'::error file={__name__}::{escape(msg)}\n')
+
+def print_debug(msg: str):
+    sys.stdout.write(f'::debug file={__name__}::{escape(msg)}\n')
+    
+def get_input(name: str, required: bool = False, default: Optional[str] = None) -> str:
     v = os.environ.get(f'INPUT_{name.upper()}', '')
     if v == '' and default:
         v = default
     if required and v == '':
-        print_action_error(f'input required and not supplied: {name}')
+        print_error(f'input required and not supplied: {name}')
         exit(1)
     return v
 
-def delete(token, repo, comment_id) -> Tuple[str, str]:
+def delete(token, repo, comment_id):
     headers = {
         'Authorization': f'token {token}',
     }
@@ -25,23 +31,23 @@ def delete(token, repo, comment_id) -> Tuple[str, str]:
         headers=headers,
     )
     if resp.status_code != HTTPStatus.NO_CONTENT:
-        print_action_error(f'cannot delete comment')
-        print_action_debug(f'status code: {resp.status_code}')
-        print_action_debug(f'response body: {resp.text}')
+        print_error(f'cannot delete comment')
+        print_debug(f'status code: {resp.status_code}')
+        print_debug(f'response body: {resp.text}')
         exit(1)
 
 def main():
     repo = os.environ['GITHUB_REPOSITORY']
-    action_type = get_action_input('type', required=True)
-    token = get_action_input('token', required=True)
-    body = get_action_input('body', required=True)
-    bad_text = get_action_input('bad_text', required=True)
-    comment_id = get_action_input('comment_id')
-    issue_number = get_action_input('issue_number')
+    action_type = get_input('type', required=True)
+    token = get_input('token', required=True)
+    body = get_input('body', required=True)
+    bad_text = get_input('bad_text', required=True)
+    comment_id = get_input('comment_id')
+    issue_number = get_input('issue_number')
 
     if bad_text in body:
-        print_action_error(f"Found {bad_text} in comment. Disallowed!")
-        _id, _body = delete(token, repo, comment_id)
+        print_error(f"Found {bad_text} in comment. Disallowed!")
+        delete(token, repo, comment_id)
         exit(1)
         
 if __name__ == '__main__':
